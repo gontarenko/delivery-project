01344e226860cfa6df3307a3254bc52e3665c6a3
gontarenko <gontarenko.dev@gmail.com>
2024-06-01 16:43:12 +0300

    test pull request

--- services/restaurant-service/restaurant-service-app/src/main/java/com/deliveryproject/restaurantserviceapp/RestaurantServiceAppApplication.java
+++ services/restaurant-service/restaurant-service-app/src/main/java/com/deliveryproject/restaurantserviceapp/RestaurantServiceAppApplication.java
@@ -3,6 +3,7 @@ package com.deliveryproject.restaurantserviceapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test
 @SpringBootApplication
 public class RestaurantServiceAppApplication {
 
